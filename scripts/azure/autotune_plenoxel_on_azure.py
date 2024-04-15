02c1ba85bf52c0f7785b151b7f4353b38896eaa4
etienne chassaing <etienne.chassaing@schindler.com>
2024-04-15 13:28:26 +0200

    Add scripts to train on Azure Adds scripts to train/autotune/eval on azure

---
+++ scripts/azure/autotune_plenoxel_on_azure.py
@@ -0,0 +1,60 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+import tyro
+from azure.ai.ml import Input, command
+from azure.ai.ml.constants import AssetTypes, InputOutputModes
+from azure_ml_utils.azure_connect import get_client
+
+
+@dataclass
+class EvalParameters:
+    environment_version: str = "latest"
+    experiment_name: str = "plenoxel-autotune"
+    """Experiment name in azure"""
+
+    model_version: str = "1"
+
+    @property
+    def environment(self) -> str:
+        return "plenoxel-310"
+
+
+def main() -> None:
+    params = tyro.cli(EvalParameters)
+
+    ml_client = get_client()
+
+    data_asset = ml_client.data.get("exhibition_building_plenoxel", version="1")
+
+    job_name = "autotune-plenoxel--" + datetime.now().strftime("%d-%m-%Y-%H%M%S")
+
+    job = command(
+        inputs={
+            "data": Input(
+                path=data_asset.id,
+                type=AssetTypes.URI_FOLDER,
+                mode=InputOutputModes.MOUNT,
+            )
+        },
+        code=".",
+        environment=params.environment + "@" + params.environment_version,
+        compute="voxel-a100",
+        command=(
+            "python3.10 plenoxels/opt/find_best_config.py "
+            "--data_root ${{inputs.data}} "
+            "--train_root ./ "
+            "-g '0' plenoxels/opt/tasks/multi_config.json "
+        ),
+        experiment_name=params.experiment_name + "plenoxel",
+        display_name=job_name,
+        name=job_name,
+    )
+
+    returned_job = ml_client.jobs.create_or_update(job)
+
+    assert returned_job.services is not None
+
+
+if __name__ == "__main__":
+    main()
